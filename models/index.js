15ed743d8e66623ff8d6110a7a808066fbb9a809
Cullen Knott <cullenp.knott@gmail.com>
2023-08-21 14:54:31 -0700

    added feature to view posts on the home page

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
-const User = require('./User');
-const Blogs = require('./Blog');
+const User = require("./User");
+const Blog = require("./Blog");
 
-User.hasMany(Blogs, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
+User.hasMany(Blog, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
-Blogs.belongsTo(User, {
-    foreignKey: 'user_id',
+Blog.belongsTo(User, {
+  foreignKey: "user_id",
 });
 
-module.exports = { User, Blogs };
+module.exports = { User, Blog };
